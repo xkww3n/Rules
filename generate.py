2bb4b17dfa366f5601725ce241a17df37053e926
xkww3n <xkww3n@outlook.com>
2024-03-31 14:55:59 +0800

    Add debug env var.

--- generate.py
+++ generate.py
@@ -1,9 +1,13 @@
 import logging.config
+from os import environ
 
 from workers import *
 
 logging.config.fileConfig("logging.ini")
 logger = logging.getLogger("root")
+
+if environ.get("DEBUG"):
+    logger.setLevel("DEBUG")
 
 reject_exclude.generate()
 domestic_domain.generate()
