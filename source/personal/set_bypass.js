a5600062bd505403e08de96e4af280c16adac657
xkww3n <xkww3n@outlook.com>
2024-09-20 11:53:33 +0800

    fix bug

--- source/personal/set_bypass.js
+++ source/personal/set_bypass.js
@@ -5,28 +5,28 @@ const GLOBAL_WIFI = [
     "McDonald's"
 ];
 
-var bypass_cellular = true;
-var bypass_wifi = true;
+var bypass_cellular = "true";
+var bypass_wifi = "true";
 
 try {
     if ($intent.parameter === "内地") {
-        bypass_cellular = false;
+        bypass_cellular = "false";
     }
     else if ($intent.parameter === "f0rC3") {  //force bypass regardless of network
-        $persistentStore.write(true, "bypass_wifi");
+        $persistentStore.write("true", "bypass_wifi");
     }
     else {
-        bypass_cellular = true;
+        bypass_cellular = "true";
     }
     $persistentStore.write(bypass_cellular, "bypass_cellular");
 }
 catch (err) {  // triggered by network-changed event, no `$intent` passed in
     if ($network.wifi.ssid) {
         if (GLOBAL_WIFI.includes($network.wifi.ssid)){
-            bypass_wifi = true;
+            bypass_wifi = "true";
         }
         else {
-            bypass_wifi = false;
+            bypass_wifi = "false";
         }
         $persistentStore.write(bypass_wifi, "bypass_wifi");
     }
