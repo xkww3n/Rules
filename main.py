89aaaf02ab11aaf5d0099b94b346e8ca9995a301
xkww3n <xkww3n@outlook.com>
2025-03-29 00:09:00 +0800

    Make pylance happy

--- main.py
+++ main.py
@@ -13,6 +13,12 @@ workers = Path("workers").rglob("*.py")
 for worker in workers:
     worker_name = worker.stem
     spec = spec_from_file_location(worker_name, Path("workers")/worker.name)
+    if spec is None:
+        logger.warning(f"Could not load spec for worker {worker_name}")
+        continue
     module = module_from_spec(spec)
+    if spec.loader is None:
+        logger.warning(f"No loader found for worker {worker_name}")
+        continue
     spec.loader.exec_module(module)
     module.build()
